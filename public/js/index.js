6d1103963b10725d8db21cadd615fda1fa85e98c
Tam Le <letam210795@gmail.com>
2018-04-19 16:45:48 +0700

    Greet new user and alert other

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
+    /* socket.emit('createMessage', {
         to: 'jen@example.com',
         text: 'Hello from FE'
-    });
+    }); */
 });
 
 socket.on('disconnect', function () {
