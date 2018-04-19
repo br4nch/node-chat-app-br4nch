dd8df8b70339ce885b1656390fcd689af5be6041
Tam Le <letam210795@gmail.com>
2018-04-19 16:11:35 +0700

    add emit event for server & fe

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newEmail', {
+        from: 'ab@example.com',
+        text: 'Hello',
+        createdAt: new Date()
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail);
+    })
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
